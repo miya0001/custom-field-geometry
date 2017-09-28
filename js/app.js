fd3f0aa8a72ae1b76f9d9eb5433192a4d226c3c8
miya0001 <miya@wpist.me>
2017-09-28 15:48:14 +0900

    remove console.log

--- js/app.js
+++ js/app.js
@@ -14,9 +14,7 @@ const latlng = localStorage.getItem( 'location' )
 if ( latlng ) {
   [ config.zoom, config.lat, config.lng ] = latlng.split( ',' )
 }
-console.log(custom_field_geometry_id)
-console.log( jQuery( '#' + custom_field_geometry_id ).length )
-console.log( jQuery( '#' + custom_field_geometry_id + ' .lat' ).length )
+
 // Override the lat and lng from post_meta
 if ( jQuery( '#' + custom_field_geometry_id + ' .lat' ).val() && jQuery( '#' + custom_field_geometry_id + ' .lng' ).val() ) {
 	config.lat  = jQuery( '#' + custom_field_geometry_id + ' .lat' ).val();
